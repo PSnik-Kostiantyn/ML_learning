46e85a527095cf41698a12b760cff12d2896e7ad
Kostiantyn <sinkkostia@gmail.com>
2025-02-06 23:45:13 +0200

    Small refactoring

--- welcome/randomfirst.py
+++ welcome/randomfirst.py
@@ -11,7 +11,6 @@ import os.path as osp
 import glob
 import textwrap
 import warnings
-import sklearn as sln
 
 warnings.filterwarnings("ignore")
 few_rolls = np.random.randint(1, 7, size=10)
@@ -20,5 +19,5 @@ few_counts = np.histogram(few_rolls, bins=np.arange(.5, 7.5))[0]
 many_counts = np.histogram(many_rolls, bins=np.arange(.5, 7.5))[0]
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.bar(np.arange(1, 7), few_counts)
-ax2.bar(np.arange(1, 7), many_counts);
+ax2.bar(np.arange(1, 7), many_counts)
 plt.show()

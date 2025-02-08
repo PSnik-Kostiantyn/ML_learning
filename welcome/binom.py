02db721d8b08d190a451d24e77d4231bc1cd3846
Kostiantyn <sinkkostia@gmail.com>
2025-02-08 20:29:10 +0200

    Shvaiko ai  ai ai

--- welcome/binom.py
+++ welcome/binom.py
@@ -9,5 +9,5 @@ for flips in [5, 10, 20, 40, 80]:
     our_distribution = b.pmf(success, flips, .5)
     plt.hist(success, flips, weights=our_distribution)
 
-plt.xlim(0, 55);
+plt.xlim(0, 55)
 plt.show()
